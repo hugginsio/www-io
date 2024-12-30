2f4ed41df0ac0c67f1412475418cd928753a3182
Kyle Huggins <kyle@huggins.io>
2024-12-29 18:38:48 -0800

    feat: redo landing with card layout

---
+++ layouts/single.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="h-full w-full colors-primary dark:colors-primary-invert" id="layout-default">
+        <div class="max-w-3xl p-4 mx-auto">
+            <div class="flex flex-col space-y-4">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
