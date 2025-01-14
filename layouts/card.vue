bd101167d1a3e10ee2772c103feb8972468526ac
Kyle Huggins <kyle@huggins.io>
2025-01-13 20:38:55 -0600

    fix: error page colors

--- layouts/card.vue
+++ layouts/card.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="h-full w-full flex flex-col justify-end max-w-4xl lg:py-28 mx-auto" id="layout-card">
+    <div
+        class="h-full w-full colors-primary dark:colors-primary-invert p-4 flex flex-col justify-end max-w-4xl lg:py-28 mx-auto"
+        id="layout-card"
+    >
         <slot />
     </div>
 </template>
